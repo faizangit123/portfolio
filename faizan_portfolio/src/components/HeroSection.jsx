b301296b89dc9a784888b04b522ef321ca70a33c
MD Faizan <faizanrock705@gmail.com>
2026-01-05 16:04:57 +0530

    Initial commit

--- faizan_portfolio/src/components/HeroSection.jsx
+++ faizan_portfolio/src/components/HeroSection.jsx
@@ -34,7 +34,7 @@ const HeroSection = () => {
               animate={{ rotate: [0, 14, -8, 14, -4, 10, 0] }}
               transition={{ duration: 2.5, repeat: Infinity, repeatDelay: 3 }}
             >
-              👋
+              
             </Motion.span>
           </h1>
         </Motion.div>
